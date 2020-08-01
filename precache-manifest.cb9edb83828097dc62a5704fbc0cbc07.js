92d7c7e65cdd2c072c30fd5635dbf262b8e90884
Matei Mircea <mrc_matei@yahoo.com>
2020-08-01 19:13:20 +0300

    Updates

---
+++ precache-manifest.cb9edb83828097dc62a5704fbc0cbc07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e76229d106ef290c6b8bd2de99ae493",
+    "url": "/Chuck/index.html"
+  },
+  {
+    "revision": "902a37384d0075ccdeb6",
+    "url": "/Chuck/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7f14667409f8ca810456",
+    "url": "/Chuck/static/css/main.4ad4ec73.chunk.css"
+  },
+  {
+    "revision": "902a37384d0075ccdeb6",
+    "url": "/Chuck/static/js/2.7178a252.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Chuck/static/js/2.7178a252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f14667409f8ca810456",
+    "url": "/Chuck/static/js/main.a7b1cbfa.chunk.js"
+  },
+  {
+    "revision": "cceaaa69ac1695b0f31d",
+    "url": "/Chuck/static/js/runtime-main.a46e1295.js"
+  },
+  {
+    "revision": "a9f147c00718540fc00cb88452def3ef",
+    "url": "/Chuck/static/media/logo.a9f147c0.jpg"
+  }
+]);

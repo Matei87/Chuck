05c90c47ab50f2f0d2c5584adb557179e602ed8c
Matei87 <mrc_matei@yahoo.com>
2024-07-29 17:40:32 +0300

    upgraded packages

--- src/App.js
+++ src/App.js
@@ -3,178 +3,134 @@ import Header from './components/Header/Header';
 import JokeCard from './components/JokeCard/JokeCard';
 import Footer from './components/Footer/Footer';
 
-import { Tab, Tabs, Form } from 'react-bootstrap';
-import Badge from 'react-bootstrap/Badge'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import './App.css';
-
 
 function App() {
   const [jokes, setJokes] = useState([]);
-  const [jokesToShow, setJokesToShow] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [filterCat, setFilterCat] = useState([]);
   const [likedJokes, setLikedJokes] = useState([]);
-  const [currentTab, setCurrentTab] = useState('home');
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState({});
-
+  const [reload, setReload] = useState(false);
 
   useEffect(() => {
-    fetch('https://api.chucknorris.io/jokes/')
-      .then(res => res.json())
-      .then(data => {
-        setJokes(data.value);
-        setJokesToShow(data.value.slice(0, 10))
-      }).catch(err => setError(err))
+    fetch(`https://api.chucknorris.io/jokes/random`)
+      .then((res) => res.json())
+      .then((data) => {
+        setJokes([data]);
+      })
+      .catch((err) => console.log(err));
 
-
-    fetch(`https://api.icndb.com/categories`)
-      .then(res => res.json())
-      .then(res => {
-        setCategories(res.value);
-        setFilterCat(res.value);
-      }).catch(err => setError(err))
-
-  }, [])
-
-
-  useEffect(() => {
-    const addMoreJokes = () => {
-      setLoading(true);
-      setTimeout(() => {
-        setJokesToShow(jokes.slice(0, jokesToShow.length + 10));
-      }, 500)
-    }
-
-    const observeElement = (bottomJoke) => {
-      if (!bottomJoke) {
-        return;
-      }
-      const observer = new IntersectionObserver(entries => {
-        if (entries[0].isIntersecting === true) {
-          addMoreJokes();
-          observer.unobserve(bottomJoke);
-        }
-      }, { threshold: 1, })
-
-      observer.observe(bottomJoke);
-    }
-
-    const bottomJokeEl = document.getElementById(`joke-${jokesToShow.length - 1}`);
-    observeElement(bottomJokeEl);
-  }, [jokesToShow])
-
-
-  const likeJoke = (id) => {
-    if (likedJokes.find(j => j.id === id)) { return; }
-    const likedJoke = jokes.find(j => j.id === id)
-    setLikedJokes([likedJoke, ...likedJokes]);
-  }
-
-  const unlikeJoke = (id) => {
-    const newLikedJokes = likedJokes.filter(j => j.id !== id)
-    setLikedJokes(newLikedJokes)
-  }
-
-
-  //loads the liked jokes from local storage on refresh/start new session
-  useEffect(() => {
     const localJokes = localStorage.getItem('jokes');
     if (localJokes) {
-      setLikedJokes(JSON.parse(localJokes))
+      setLikedJokes(JSON.parse(localJokes));
     }
-  }, [])
+  }, [reload]);
+
+  const likeJoke = (id) => {
+    if (likedJokes.find((j) => j.id === id)) {
+      return;
+    }
+    const likedJoke = jokes.find((j) => j.id === id);
+    setLikedJokes([likedJoke, ...likedJokes]);
+  };
+
+  const unlikeJoke = (id) => {
+    const newLikedJokes = likedJokes.filter((j) => j.id !== id);
+    setLikedJokes(newLikedJokes);
+  };
 
   //stores the liked jokes to local storage
   useEffect(() => {
     localStorage.setItem('jokes', JSON.stringify(likedJokes));
-  })
-
-  const onChangeCategories = (e) => {
-    const category = e.target.name;
-    if (filterCat.includes(category)) {
-      // if found then remove
-      const filterCategoriesCopy = [...filterCat];
-      const categoryIndex = filterCategoriesCopy.indexOf(category);
-      filterCategoriesCopy.splice(categoryIndex, 1);
-      setFilterCat(filterCategoriesCopy);
-    } else {
-      //else add it
-      setFilterCat([...filterCat, category])
-    }
-  }
-
-  const categoryMatch = (jokeCategories) => {
-    for (let i = 0; i < jokeCategories.length; i++) {
-      if (filterCat.includes(jokeCategories[i])) { return true; }
-    }
-    return false;
-  }
+  });
 
   return (
     <>
       <Header />
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <ul className='nav nav-pills mb-3' id='pills-tab' role='tablist'>
+              <li className='nav-item' role='presentation'>
+                <button
+                  className='nav-link active'
+                  id='pills-home-tab'
+                  data-bs-toggle='pill'
+                  data-bs-target='#pills-home'
+                  type='button'
+                  role='tab'
+                  aria-controls='pills-home'
+                  aria-selected='true'
+                >
+                  Home
+                </button>
+              </li>
+              <li className='nav-item' role='presentation'>
+                <button
+                  className='nav-link position-relative'
+                  id='pills-likes-tab'
+                  data-bs-toggle='pill'
+                  data-bs-target='#pills-likes'
+                  type='button'
+                  role='tab'
+                  aria-controls='pills-likes'
+                  aria-selected='false'
+                >
+                  Likes
+                  <span className='position-absolute start-100 translate-middle badge rounded-pill bg-danger'>
+                    {likedJokes.length > 0 ? likedJokes.length : null}
+                  </span>
+                </button>
+              </li>
+            </ul>
 
-            <Tabs activeKey={currentTab} onSelect={(k) => setCurrentTab(k)} >
-              <Tab eventKey="home" title="Home" />
-              <Tab eventKey="profile" title={<span>Likes
-                <Badge pill
-                  className="edited-badge-pill"
-                  variant="primary">
-                  {likedJokes.length > 0 ? likedJokes.length : null}
-                </Badge></span>}
-              />
-            </Tabs>
-
-            <div hidden={currentTab !== 'home'} >
-              {/* Category filters */}
-              <Form inline>
-                {categories.map(cat => (
-                  <div key={cat} className="mr-3 mb-3">
-                    < Form.Check
-                      label={cat}
-                      type="checkbox"
-                      name={cat}
-                      checked={filterCat.includes(cat)}
-                      onChange={onChangeCategories}
+            <div className='tab-content' id='pills-tabContent'>
+              <div
+                className='tab-pane fade show active'
+                id='pills-home'
+                role='tabpanel'
+                aria-labelledby='pills-home-tab'
+                tabIndex='0'
+              >
+                <div>
+                  {/* Joke cards */}
+                  {jokes.map((joke, index) => (
+                    <JokeCard
+                      key={joke.id}
+                      jokeProp={joke}
+                      laikeJoke={likeJoke}
+                      unlaikeJoke={unlikeJoke}
+                      index={index}
                     />
-                  </div>
-                ))}
-              </Form>
+                  ))}
+                </div>
+                <button
+                  className='reload-button'
+                  onClick={() => setReload((prev) => !prev)}
+                >
+                  New Joke
+                </button>
+              </div>
 
-              {/* Joke cards */}
-              {jokesToShow.map((joke, index) => (
-                joke.categories.length === 0 || categoryMatch(joke.categories) ?
-                  <JokeCard
-                    key={joke.id}
-                    jokeProp={joke}
-                    laikeJoke={likeJoke}
-                    unlaikeJoke={unlikeJoke}
-                    index={index}
-                  /> : ""
-
-
-              ))}
-              {loading && <div className="spinner-grow text-dark" role="status">
-                <span className="sr-only">Loading...</span>
-              </div>}
+              <div
+                className='tab-pane fade'
+                id='pills-likes'
+                role='tabpanel'
+                aria-labelledby='pills-likes-tab'
+                tabIndex='0'
+              >
+                <div>
+                  {likedJokes.map((joke) => (
+                    <JokeCard
+                      key={joke.id}
+                      jokeProp={joke}
+                      laikeJoke={likeJoke}
+                      unlaikeJoke={unlikeJoke}
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
-
-            <div hidden={currentTab === 'home'} >
-              {likedJokes.map(joke => (
-                <JokeCard
-                  key={joke.id}
-                  jokeProp={joke}
-                  laikeJoke={likeJoke}
-                  unlaikeJoke={unlikeJoke}
-                />
-              ))}
-            </div>
-
           </div>
         </div>
       </div>
--- src/components/JokeCard/JokeCard.js
+++ src/components/JokeCard/JokeCard.js
@@ -1,24 +1,53 @@
-import React, { Component } from 'react';
-import './JokeCard.css';
+import React from 'react';
 
-class JokeCard extends Component {
-    render() {
-        const { jokeProp, unlaikeJoke, laikeJoke, index } = this.props;
+const JokeCard = ({ jokeProp, unlaikeJoke, laikeJoke, index }) => {
+  return (
+    <div
+      className='card rounded-0'
+      style={{ marginBottom: '10px' }}
+      id={`joke-${index}`}
+    >
+      <div className='card-body'>
+        {jokeProp.categories.length > 0 ? (
+          jokeProp.categories.map((cat) => (
+            <span
+              className='badge badge-pill badge-primary'
+              key={cat}
+              style={{ color: '#F7F7F7', background: 'crimson' }}
+            >
+              {cat}
+            </span>
+          ))
+        ) : (
+          <span
+            className='badge badge-pill badge-primary'
+            style={{ color: '#F7F7F7', background: '#3d6ad6' }}
+          >
+            regular
+          </span>
+        )}
 
-        return (
-            <div className="card" style={{ marginBottom: '10px' }} id={`joke-${index}`}>
-                <div className="card-body">
-                    {jokeProp.categories.length > 0 ? jokeProp.categories.map(cat => (
-                        <span className="badge badge-pill badge-primary" key={cat} style={{ color: '#F7F7F7', background: 'crimson' }}>{cat}</span>
-                    )) : <span className="badge badge-pill badge-primary" style={{ color: '#F7F7F7', background: '#3d6ad6' }}>regular</span>}
-
-                    <p className="card-text" style={{ color: 'crimson' }}>{jokeProp.joke}</p>
-                    <button type="button" className="btn btn-primary" style={{ marginRight: '10px' }} onClick={() => laikeJoke(jokeProp.id)} ><i className="fas fa-thumbs-up" /></button>
-                    <button type="button" className="btn btn-primary" onClick={() => unlaikeJoke(jokeProp.id)} ><i className="fas fa-thumbs-down" /></button>
-                </div>
-            </div>
-        )
-    }
-}
+        <p className='card-text' style={{ color: 'crimson' }}>
+          {jokeProp.value}
+        </p>
+        <button
+          type='button'
+          className='btn btn-primary'
+          style={{ marginRight: '10px' }}
+          onClick={() => laikeJoke(jokeProp.id)}
+        >
+          <i className='fas fa-thumbs-up' />
+        </button>
+        <button
+          type='button'
+          className='btn btn-primary'
+          onClick={() => unlaikeJoke(jokeProp.id)}
+        >
+          <i className='fas fa-thumbs-down' />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default JokeCard;
